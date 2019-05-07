9f12e8edfed25006041f51ed21541d5074651233
zizy <nuaanjzizy@163.com>
2019-05-07 10:57:01 +0800

    ldd demo globalmem ok

--- Linux_Driver_Develop/globalmem/driver.c
+++ Linux_Driver_Develop/globalmem/driver.c
@@ -4,10 +4,16 @@
 #include <linux/cdev.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
+#include <linux/kernel.h>
 
 #define GLOBALMEM_SIZE      0X1000
 #define MEM_CLEAR           0X1
 #define GLOBALMEM_MAJOR     230
+
+#define MULTI_DEV
+#ifdef MULTI_DEV
+#define DEVICE_NUM          10
+#endif
 
 static int globalmem_major = GLOBALMEM_MAJOR;
 module_param(globalmem_major, int, S_IRUGO);
@@ -19,13 +25,124 @@ struct globalmem_dev {
 
 struct globalmem_dev *globalmem_devp;
 
+static int globalmem_open(struct inode *inode, struct file *filp)
+{
+    #ifdef MULTI_DEV
+    struct globalmem_dev *dev = container_of(inode->i_cdev, struct globalmem_dev, cdev);
+    filp->private_data = dev;
+    #else
+    filp->private_data = globalmem_devp;
+    #endif
+    return 0;
+}
+
+static int globalmem_release(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+static long globalmem_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) 
+{
+    struct globalmem_dev *dev = filp->private_data;
+
+    switch(cmd) {
+        case MEM_CLEAR: {
+            memset(dev->mem, 0, GLOBALMEM_SIZE);
+            printk(KERN_INFO "globalmem is set to zero\n");
+            break;
+        }
+        default:
+            return -EINVAL;
+    }
+    return 0;
+}
+
 static ssize_t globalmem_read(struct file *filp, char __user *buf, size_t size, loff_t *ppos)
 {
     unsigned long p = *ppos;
     unsigned int count = size;
     int ret = 0;
-    struct globalmem_dev *dev = flip->private_data;
+    struct globalmem_dev *dev = filp->private_data;
+    
+    if(p >= GLOBALMEM_SIZE) 
+        return 0;
+    if(count > GLOBALMEM_SIZE - p) 
+        count = GLOBALMEM_SIZE - p;
+    
+    if(copy_to_user(buf, dev->mem + p, count)) {
+        ret = -EFAULT;
+    } else {
+        *ppos += count;
+        ret = count;
+
+        printk(KERN_INFO "read %u byte(s) from %lu\n", count, p);
+    }
+
+    return ret;
 }
+
+static ssize_t globalmem_write(struct file *filp, const char __user *buf, size_t size , loff_t *ppos)
+{
+    unsigned long p = *ppos;
+    unsigned int count = size;
+    int ret = 0;
+    struct globalmem_dev *dev = filp->private_data;
+
+    if(p >= GLOBALMEM_SIZE) 
+        return 0;
+    if(count > GLOBALMEM_SIZE - p)
+        count = GLOBALMEM_SIZE - p;
+
+    if(copy_from_user(dev->mem + p, buf, count))
+        ret = -EFAULT;
+    else {
+        *ppos += count;
+        ret = count;
+
+        printk(KERN_INFO "written %u byte(s) from %lu\n", count, p);
+    }
+
+    return ret;
+}
+
+static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
+{
+    loff_t ret = 0;
+    switch(orig) {
+        case 0: {
+            if(offset < 0) {
+                ret = -EINVAL;
+                break;
+            }
+            if((unsigned int) offset > GLOBALMEM_SIZE) {
+                ret = -EINVAL;
+                break;
+            }
+            filp->f_pos = (unsigned int) offset;
+            ret = filp->f_pos;
+            break;
+        }
+        case 1: {
+            if((filp->f_pos + offset) > GLOBALMEM_SIZE) {
+                ret = -EINVAL;
+                break;
+            }
+            if((filp->f_pos + offset) < 0) {
+                ret = -EINVAL;
+                break;
+            }
+            filp->f_pos += offset;
+            ret = filp->f_pos;
+            break;
+        }
+        default: {
+            ret = -EINVAL;
+            break;
+        }
+    }
+    return ret;
+}
+
 
 static const struct file_operations globalmem_fops = {
     .owner = THIS_MODULE,
@@ -52,26 +169,74 @@ static void globalmem_setup_cdev(struct globalmem_dev *dev, int index)
 static int __init globalmem_init(void)
 {
     int ret;
+    int i;
     dev_t devno = MKDEV(globalmem_major, 0);
+    printk("--------%s--------\n", __func__);
     if(globalmem_major)
+        #ifdef MULTI_DEV
+        ret = register_chrdev_region(devno, DEVICE_NUM, "globalmem");
+        #else
         ret = register_chrdev_region(devno, 1, "globalmem");
+        #endif
     else {
+        #ifdef MULTI_DEV
+        ret = alloc_chrdev_region(&devno, 0, DEVICE_NUM, "globalmem");
+        #else
         ret = alloc_chrdev_region(&devno, 0, 1, "globalmem");
+        #endif
         globalmem_major = MAJOR(devno);
     }
     if(ret < 0) 
         return ret;
-    
+    #ifdef MULTI_DEV
+    globalmem_devp = kzalloc(sizeof(struct globalmem_dev) * DEVICE_NUM, GFP_KERNEL);
+    #else
     globalmem_devp = kzalloc(sizeof(struct globalmem_dev), GFP_KERNEL);
+    #endif
     if(!globalmem_devp) {
         ret = -ENOMEM;
         goto fail_malloc;
     }
-
+    #ifdef MULTI_DEV
+    for(i = 0; i < DEVICE_NUM; i++) {
+        globalmem_setup_cdev(globalmem_devp + i, i);
+    }
+    #else
     globalmem_setup_cdev(globalmem_devp, 0);
+    #endif
     return 0;
     fail_malloc:
+    #ifdef MULTI_DEV
+    unregister_chrdev_region(devno, DEVICE_NUM);
+    #else
     unregister_chrdev_region(devno, 1);
+    #endif 
     return ret;
 }
 module_init(globalmem_init);
+
+static void __exit globalmem_exit(void)
+{
+    int i;
+    printk("--------%s--------\n", __func__);
+    #ifdef MULTI_DEV
+    for(i = 0; i < DEVICE_NUM; i++) {
+        cdev_del(&(globalmem_devp + i)->cdev);
+    }
+    kfree(globalmem_devp);
+    unregister_chrdev_region(MKDEV(globalmem_major, 0), DEVICE_NUM);
+    #else
+    cdev_del(&globalmem_devp->cdev);
+    kfree(globalmem_devp);
+    unregister_chrdev_region(MKDEV(globalmem_major, 0), 1);
+    #endif
+}
+module_exit(globalmem_exit);
+
+MODULE_AUTHOR("ZIZY");
+MODULE_LICENSE("GPL v2");
+
+/**
+ * !! mknod /dev/globalmem c 230 0
+ * 
+ */
