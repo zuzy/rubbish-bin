40f8c909bc66e9fdea3c87b1c689b7cddd999bf0
zizy <nuaanjzizy@163.com>
2019-06-14 08:52:58 +0800

    system

--- c/systime/get_set_systime.c
+++ c/systime/get_set_systime.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+
+void print_time()
+{
+    char buf[1024] = {0};
+    FILE *f = popen("date", "r");
+    int nread = fread(buf, 1, sizeof(buf), f);
+    if(nread > 0) {
+        printf("get system %s\n", buf);
+    } else {
+        printf("get time failed\n");
+    }
+    pclose(f);
+}
 
 int main(int argc, char *argv[])
 {
-  
-  return 0;
+    print_time();
+    time_t t = (time_t)random();
+    stime(&t);
+    print_time();
+    return 0;
 }
