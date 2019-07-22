cdb77a433256627de47e63e1ef528d9e8614110c
zizy <nuaanjzizy@163.com>
2019-07-22 11:20:40 +0800

    string format to hex && automake demo here

---
+++ c/tmp/str_to_hex.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static int custom_to_hex(char *input, char *ret, int length)
+{
+    int retlen = 0;
+    if(!input || !length) goto End;
+
+    size_t ilen = strlen(input);
+    if(ilen > 100 ||  ilen == 0) 
+        goto End;
+    retlen = ilen >> 1;
+    
+    char *p = input;
+    unsigned char *r = ret;
+    unsigned char flag = 1;
+    while(*p) {
+        unsigned char val = 0;
+        switch (*p)
+        {
+        case '0'...'9': {
+            val = *p - '0';
+            break;
+        }
+        case 'A'...'Z': {
+            val = *p - 'A' + 0x0a;
+            break;
+        }
+        case 'a'...'z': {
+            val = *p - 'a' + 0x0a;
+            break;
+        }
+        default:
+            goto End_of_while;
+        }
+        if(flag) {
+            *r = val;
+        } else {
+            *r = (*r << 4) | val;
+            ++r;
+        }
+        if(length < (char *)r - ret) {
+            return length;
+        }
+        flag = !flag;
+        End_of_while:
+        ++p;
+    }
+    if(!flag) {
+        *r <<= 4;
+    }
+    End:
+        return retlen;
+}
+
+int main(int argc, char *argv[])
+{
+    char *s = "123456 78901";
+    char tmp[50] = {};
+    int to = custom_to_hex(s, tmp, 50);
+    printf("ret %s\n", s);
+    printf("to %d\n", to);
+    int i = 0;
+    for(; i < to; i++) {
+        printf(" %d: %02x ", i, tmp[i]);
+    }
+    printf("\n");
+    return 0;
+}
