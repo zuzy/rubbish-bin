1c6c9118310dadfd0c0e7fb5c0ecdbf4f1fe42ee
zizy <nuaanjzizy@163.com>
2019-07-29 18:03:05 +0800

    uart in progress

--- libuv/libuv_sample/ch2-fs/uart.c
+++ libuv/libuv_sample/ch2-fs/uart.c
@@ -3,21 +3,59 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <uv.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <termios.h>
-#include <uv.h>
-
-char *path = "/dev/ttyUSB0";
 
 
-static int _serial_cfg(int fd, int baud)
+void on_read(uv_fs_t *req);
+
+uv_fs_t open_req;
+uv_fs_t read_req;
+uv_fs_t write_req;
+uv_fs_t write_to_req;
+
+static char buffer[1024];
+
+static uv_buf_t iov;
+
+struct _st_serial{
+    int fd;
+    int send_timer;
+    char dev[256];
+    int baud;
+    int max_size;
+} serial_handle = {
+    -1,
+    -1,
+    "/dev/ttyUSB0",
+    115200,
+    128,
+};
+
+
+static int _serial_open(void)
 {
+    serial_handle.fd = open(serial_handle.dev, O_RDWR|O_NOCTTY|O_NDELAY);
+    fcntl(serial_handle.fd, F_SETFL, 0);
+    return 0;
+Error:
+    if(serial_handle.fd >= 0) {
+        close(serial_handle.fd);
+        serial_handle.fd = -1;
+    }
+    return -1;
+}
+
+static int _serial_cfg(uv_fs_t *req)
+{
+    fcntl(req->result, F_SETFL, 0);
     struct termios opt;
-    if(tcgetattr(fd, &opt) != 0) goto Error;
-    cfsetispeed(&opt, baud);
-    cfsetospeed(&opt, baud);
+    tcgetattr(req->result, &opt);
+    cfsetispeed(&opt, serial_handle.baud);
+    cfsetospeed(&opt, serial_handle.baud);
     opt.c_cflag |= CLOCAL | CREAD;
     opt.c_cflag &= ~(CRTSCTS | CSIZE);
 
@@ -34,32 +72,21 @@ static int _serial_cfg(int fd, int baud)
     opt.c_cc[VTIME] = 0;
     opt.c_cc[VMIN] = 0;
 
-    tcflush(fd, TCIFLUSH);
+    tcflush(req->result, TCIFLUSH);
     
-    if(tcsetattr(fd, TCSANOW, &opt) != 0) goto Error;
+    tcsetattr(req->result, TCSANOW, &opt);
+    printf("open ok\n");
     
-    return fd;
+    return 0;
 
 Error:
-    if(fd >= 0) {
-        close(fd);
-        fd = -1;
+    if(open_req.result >= 0) {
+        close(open_req.result);
     }
     return -1;
 }
 
 
-
-void on_read(uv_fs_t *req);
-
-uv_fs_t open_req;
-uv_fs_t read_req;
-uv_fs_t write_req;
-uv_fs_t write_to_req;
-
-static char buffer[1024];
-
-static uv_buf_t iov;
 
 void on_write(uv_fs_t *req) {
     if (req->result < 0) {
@@ -70,7 +97,13 @@ void on_write(uv_fs_t *req) {
     }
 }
 
+void wait_for_a_while(uv_idle_t *handle)
+{
+    usleep(100000);
+}
+
 void on_read(uv_fs_t *req) {
+    printf("reading \n");
     if (req->result < 0) {
         fprintf(stderr, "Read error: %s\n", uv_strerror(req->result));
     }
@@ -79,20 +112,27 @@ void on_read(uv_fs_t *req) {
     //     // synchronous
     //     uv_fs_close(uv_default_loop(), &close_req, open_req.result, NULL);
     // }
-    else if (req->result > 0) {
+    else if (req->result >= 0) {
         iov.len = req->result;
-        uv_fs_write(uv_default_loop(), &write_req, write_to_req.result, &iov, 1, -1, on_write);
+        if(req->result > 0) {
+            uv_fs_write(uv_default_loop(), &write_req, write_to_req.result, &iov, 1, -1, on_write);
+        } else {
+            uv_idle_t idle;
+            uv_idle_init(uv_default_loop(), &idle);
+            uv_idle_start(&idle,wait_for_a_while);
+        }
     }
 }
 
 void on_open(uv_fs_t *req) {
     // The request passed to the callback is the same as the one the call setup
     // function was passed.
-    assert(req == &open_req);
-    fcntl(req->result, F_SETFL, 0);
-    _serial_cfg(req->result, 115200);
+    // fcntl(req->result, F_SETFL, 0);
+    _serial_cfg(req);
+    // assert(req == &open_req);
     if (req->result >= 0) {
         iov = uv_buf_init(buffer, sizeof(buffer));
+        printf("on read!!\n");
         uv_fs_read(uv_default_loop(), &read_req, req->result,
                    &iov, 1, -1, on_read);
     }
@@ -102,9 +142,11 @@ void on_open(uv_fs_t *req) {
 }
 
 int main(int argc, char **argv) {
-    uv_fs_open(uv_default_loop(), &open_req, path, O_RDWR|O_NOCTTY|O_NDELAY, 0, on_open);
-
+    uv_fs_open(uv_default_loop(), &open_req, serial_handle.dev, O_RDWR|O_NOCTTY|O_NDELAY, 0, on_open);
+    
+    // if(argv[2] && strlen(argv[2]))
     // uv_fs_open(uv_default_loop(), &write_to_req, argv[2], O_RDWR | O_CREAT | O_APPEND, 0664, NULL);
+    // xelse 
     write_to_req.result = 1;
 
     uv_run(uv_default_loop(), UV_RUN_DEFAULT);
