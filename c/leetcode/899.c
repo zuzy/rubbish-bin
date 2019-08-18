36c9b5434302c2a266a53fcfc7c8cc829336bf7b
zizy <nuaanjzizy@163.com>
2019-08-19 00:03:20 +0800

    ...

---
+++ c/leetcode/899.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief  https://leetcode-cn.com/problems/orderly-queue/
+ * @note
+ * ? 1. 确保前K个都小于后面的;
+ * ? 2. 确保前K个都按照从小到大的顺序;
+ * @param  S: 
+ * @param  K: 
+ * @retval 
+ */
+
+char * orderlyQueue(char * S, int K)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+    // char tmp[128] = "z\0\0dxscdwadcez";
+    // char *ss = _min_of_string(tmp, 1);
+    // printf("ss %.02x %c tmp %s\n", *ss, *ss, tmp);
+
+    return 0;
+}
