65e1446adda769553071ccd5913c85c316b04289
zuzy <nuaanjzizy@163.com>
2020-01-19 17:45:41 +0800

    tree finished

---
+++ c/leetcode/707.设计链表.cpp
@@ -0,0 +1,148 @@
+/*
+ * @lc app=leetcode.cn id=707 lang=cpp
+ *
+ * [707] 设计链表
+ *
+ * https://leetcode-cn.com/problems/design-linked-list/description/
+ *
+ * algorithms
+ * Medium (23.56%)
+ * Likes:    116
+ * Dislikes: 0
+ * Total Accepted:    14.3K
+ * Total Submissions: 59.7K
+ * Testcase Example:  '["MyLinkedList","addAtHead","addAtTail","addAtIndex","get","deleteAtIndex","get"]\r\n' +
+  '[[],[1],[3],[1,2],[1],[1],[1]]\r'
+ *
+ * 设计链表的实现。您可以选择使用单链表或双链表。单链表中的节点应该具有两个属性：val 和 next。val 是当前节点的值，next
+ * 是指向下一个节点的指针/引用。如果要使用双向链表，则还需要一个属性 prev 以指示链表中的上一个节点。假设链表中的所有节点都是 0-index 的。
+ * 
+ * 在链表类中实现这些功能：
+ * 
+ * 
+ * get(index)：获取链表中第 index 个节点的值。如果索引无效，则返回-1。
+ * addAtHead(val)：在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。
+ * addAtTail(val)：将值为 val 的节点追加到链表的最后一个元素。
+ * addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val  的节点。如果 index
+ * 等于链表的长度，则该节点将附加到链表的末尾。如果 index 大于链表长度，则不会插入节点。如果index小于0，则在头部插入节点。
+ * deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。
+ * 
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * MyLinkedList linkedList = new MyLinkedList();
+ * linkedList.addAtHead(1);
+ * linkedList.addAtTail(3);
+ * linkedList.addAtIndex(1,2);   //链表变为1-> 2-> 3
+ * linkedList.get(1);            //返回2
+ * linkedList.deleteAtIndex(1);  //现在链表是1-> 3
+ * linkedList.get(1);            //返回3
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 所有val值都在 [1, 1000] 之内。
+ * 操作次数将在  [1, 1000] 之内。
+ * 请不要使用内置的 LinkedList 库。
+ * 
+ * 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// @lc code=start
+class MyLinkedList {
+private:
+    struct _list{
+        int val;
+        _list *next;
+        _list(): val(0), next(NULL) {}
+        _list(int v): val(v), next(NULL) {}
+        _list(int v, _list *n): val(v), next(n) {}
+        _list(_list *n): val(0), next(n) {}
+    };
+public:
+    _list head;
+    _list *tail;
+    int size;
+    /** Initialize your data structure here. */
+    MyLinkedList()
+    {
+        size = 0;
+        cout << head.val << endl;
+        // tail = &head;
+    }
+    
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    int get(int index) {
+        if(index < 0 || index >= size) return -1;
+        _list *p = head.next;
+        while(index--) {
+            p = p->next;
+        }
+        return p->val;
+    }
+    
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    void addAtHead(int val) {
+        _list *p = head.next;
+        _list *n = new _list(val, p);
+        head.next = n;
+        ++size;
+    }
+    
+    /** Append a node of value val to the last element of the linked list. */
+    void addAtTail(int val) {
+        tail->next = new _list(val);
+        ++size;
+    }
+    
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    void addAtIndex(int index, int val) {
+        if(index > size) return;
+        if(index == size) {
+            addAtTail(val);
+        } else {
+            _list *p = &head;
+            while(index--) {
+                p = p->next;
+            }
+            _list *q = p->next;
+            p->next = new _list(val);
+            p->next->next = q;
+            ++size;
+        }
+    }
+    
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    void deleteAtIndex(int index) {
+        if(index < 0 || index >= size) return;
+        _list *p = &head;
+        while(index--) {
+            p = p->next;
+        }
+        _list *q = p->next;
+        p->next = q->next;
+        delete q;
+        --size;
+    }
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList* obj = new MyLinkedList();
+ * int param_1 = obj->get(index);
+ * obj->addAtHead(val);
+ * obj->addAtTail(val);
+ * obj->addAtIndex(index,val);
+ * obj->deleteAtIndex(index);
+ */
+// @lc code=end
+
